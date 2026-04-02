734ce16c1374d499fe0c0b6e86cca377a1c1db9d
Sabrina Andrade <eusabrinaps@icloud.com>
2026-04-02 07:28:01 -0300

    test: Throws an exception when cancel an order with a CONCONCLUDED StatusOrder

--- src/test/java/br/ifsp/demo/domain/model/aggregate/OrderDeliveryTest.java
+++ src/test/java/br/ifsp/demo/domain/model/aggregate/OrderDeliveryTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.tracing.Status;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class OrderDeliveryTest {
     @Test
-    @DisplayName("Given that an order has been created, when canceled, the status should be canceled.")// #13
+    @DisplayName("[#13] Given that an order has been created, when canceled, the status should be canceled.")
     void ShouldReturnStatusCanceledWhenCanceled() {
         OrderDelivery order = new OrderDelivery(StatusOrder.CREATED);
         order.cancel();
@@ -16,11 +16,22 @@ public class OrderDeliveryTest {
     }
 
     @Test
-    @DisplayName("Given that an order has StatusOrder DISPATCHED, when canceled, the status should be canceled")
+    @DisplayName("[#14] Given that an order has StatusOrder DISPATCHED, when canceled, the status should be canceled")
     void ShouldReturnStatusCanceledWhenCanceledOrderDispatched() {
         OrderDelivery order = new OrderDelivery(StatusOrder.CREATED);
         order.setStatusOrder(StatusOrder.DISPATCHED);
         order.cancel();
         assertThat(order.getStatus()).isEqualTo(StatusOrder.CANCELED);
+    }
+
+    @Test
+    @DisplayName("[#15] Given that an order has StatusOrder CONCLUDED, when canceled," +
+            " must throw an exception of type IllegalStateException.")
+    void ShouldThrowIllegalStateExceptionWhenCanceledOrderConcluded() {
+        OrderDelivery order = new OrderDelivery(StatusOrder.CREATED);
+        order.setStatusOrder(StatusOrder.CONCLUDED);
+        order.cancel();
+        assertThatIllegalStateException().isThrownBy(order::cancel);
+
     }
 }
