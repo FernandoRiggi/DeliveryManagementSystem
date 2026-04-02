94e6603bf0bc39bb814ae52197076cb3bd6697e5
Sabrina Andrade <eusabrinaps@icloud.com>
2026-04-02 07:14:06 -0300

    test: cancel order with DISPATCHED status should change to CANCELED

--- src/test/java/br/ifsp/demo/domain/model/aggregate/OrderDeliveryTest.java
+++ src/test/java/br/ifsp/demo/domain/model/aggregate/OrderDeliveryTest.java
@@ -8,9 +8,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderDeliveryTest {
     @Test
-    @DisplayName("Given that an order has been created, when canceled, the status should be canceled.")
+    @DisplayName("Given that an order has been created, when canceled, the status should be canceled.")// #13
     void ShouldReturnStatusCanceledWhenCanceled() {
         OrderDelivery order = new OrderDelivery(StatusOrder.CREATED);
+        order.cancel();
+        assertThat(order.getStatus()).isEqualTo(StatusOrder.CANCELED);
+    }
+
+    @Test
+    @DisplayName("Given that an order has StatusOrder DISPATCHED, when canceled, the status should be canceled")
+    void ShouldReturnStatusCanceledWhenCanceledOrderDispatched() {
+        OrderDelivery order = new OrderDelivery(StatusOrder.CREATED);
+        order.setStatusOrder(StatusOrder.DISPATCHED);
         order.cancel();
         assertThat(order.getStatus()).isEqualTo(StatusOrder.CANCELED);
     }
