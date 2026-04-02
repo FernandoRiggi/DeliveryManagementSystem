3c3295021e32140857edd588a0f7f5bb01ce054c
Sabrina Andrade <eusabrinaps@icloud.com>
2026-04-01 21:34:58 -0300

    chore: add UnitTest, TDD and Functional annotations

---
+++ src/test/java/br/ifsp/demo/annotation/UnitTest.java
@@ -0,0 +1,9 @@
+package br.ifsp.demo.annotation;
+
+import org.junit.jupiter.api.Tag;
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("UnitTest")
+public @interface UnitTest {}
