62bc9a7979abb8e45dbd668c62e1fd802e03b607
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-13 19:43:18 -0300

    test(CreateOrderUseCaseTest): add test for issue 56 should create and save order delivery

---
+++ src/test/java/br/ifsp/demo/application/useCases/CreateOrderUseCaseTest.java
@@ -0,0 +1,55 @@
+package br.ifsp.demo.application.useCases;
+
+import br.ifsp.demo.annotation.TDD;
+import br.ifsp.demo.domain.aggregate.Customer;
+import br.ifsp.demo.domain.aggregate.DeliveryMan;
+import br.ifsp.demo.domain.aggregate.OrderDelivery;
+import br.ifsp.demo.domain.dto.CreateOrderRequest;
+import br.ifsp.demo.domain.repository.OrderDeliveryRepository;
+import br.ifsp.demo.domain.valueObject.Address;
+import br.ifsp.demo.domain.valueObject.Cep;
+import br.ifsp.demo.domain.valueObject.CustomerType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateOrderUseCaseTest {
+
+    @Mock
+    private OrderDeliveryRepository repo;
+
+    @InjectMocks CreateOrderUseCase sut;
+
+    @TDD
+    @Test
+    @DisplayName("[#56] Should create and save order delivery")
+    void ShouldCreateAndSaveOrderDelivery(){
+        CreateOrderRequest request = new CreateOrderRequest(createValidCustomer(), createValidPickupAddress(), createValidDeliveryAddress());
+
+        sut.create(request);
+
+        verify(repo, times(1)).save(any(OrderDelivery.class));
+    }
+
+    private DeliveryMan createValidDeliveryMan(){
+        return new DeliveryMan("John Doe", 10);
+    }
+
+    private Customer createValidCustomer(){
+        return new Customer("John Doe", CustomerType.REGULAR);
+    }
+
+    private Address createValidPickupAddress() {
+        return new Address("Street A", "10", "Center", "São Carlos", "SP", "Brasil", new Cep("13500-000"));
+    }
+
+    private Address createValidDeliveryAddress() {
+        return new Address("Street B", "11", "Center", "Araraquara", "SP", "Brasil", new Cep("13400-000"));
+    }
+
+}
