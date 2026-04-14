cee17ab3a9febcd8d508c377a1284ace41ba1ab5
Filipe Braga <filipebraga0505@gmail.com>
2026-04-14 16:38:40 -0300

    test: Refactoring the previous testes

--- src/test/java/br/ifsp/demo/application/useCases/ListCustomerOrdersUseCaseTest.java
+++ src/test/java/br/ifsp/demo/application/useCases/ListCustomerOrdersUseCaseTest.java
@@ -2,12 +2,14 @@ package br.ifsp.demo.application.useCases;
 
 import br.ifsp.demo.annotation.TDD;
 import br.ifsp.demo.domain.aggregate.Customer;
+import br.ifsp.demo.domain.aggregate.DeliveryMan;
 import br.ifsp.demo.domain.aggregate.OrderDelivery;
 import br.ifsp.demo.domain.repository.CustomerRepository;
 import br.ifsp.demo.domain.repository.OrderDeliveryRepository;
 import br.ifsp.demo.domain.valueObject.Address;
 import br.ifsp.demo.domain.valueObject.Cep;
 import br.ifsp.demo.domain.valueObject.CustomerType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -33,19 +35,43 @@ public class ListCustomerOrdersUseCaseTest {
     @InjectMocks
     private ListCustomerOrdersUseCase sut;
 
-    Customer customer = new Customer("Lucas", CustomerType.REGULAR);
+    private Customer customer;
+    private OrderDelivery orderDelivery;
+    private OrderDelivery orderDispatched;
+    private OrderDelivery orderEnRoute;
+    private OrderDelivery orderConcluded;
+    private OrderDelivery orderCanceled;
 
-    Address pickUpAddress = new Address("Street A", "10", "Center", "São Carlos", "SP", "Brasil", new Cep("13500-000"));
-    Address deliveryAddress = new Address("Street B", "11", "Center", "Araraquara", "SP", "Brasil", new Cep("13400-000"));
+    @BeforeEach
+    void setUp() {
+        customer = new Customer("John Doe", CustomerType.REGULAR);
 
-    OrderDelivery orderDelivery = new OrderDelivery(customer,pickUpAddress, deliveryAddress);
+        DeliveryMan deliveryMan = new DeliveryMan("John Doe", 10);
+
+        Address pickupAddress = new Address("Street A", "10", "Center", "São Carlos", "SP", "Brasil", new Cep("13500-000"));
+        Address deliveryAddress = new Address("Street B", "11", "Center", "Araraquara", "SP", "Brasil", new Cep("13400-000"));
+
+        orderDelivery = new OrderDelivery(customer, pickupAddress, deliveryAddress);
+
+        orderDispatched = new OrderDelivery(customer, pickupAddress, deliveryAddress);
+        orderDispatched.dispatch(deliveryMan);
+
+        orderEnRoute = new OrderDelivery(customer, pickupAddress, deliveryAddress);
+        orderEnRoute.startRoute();
+
+        orderConcluded = new OrderDelivery(customer, pickupAddress, deliveryAddress);
+        orderConcluded.concluded();
+
+        orderCanceled = new OrderDelivery(customer, pickupAddress, deliveryAddress);
+        orderCanceled.cancel();
+    }
 
     @TDD
     @Test
     @DisplayName("[#52] Should return all the orders from a customer")
     void shouldReturnAllOrdersFromACustomer(){
         when(customerRepository.exists(customer)).thenReturn(true);
-        when(orderDeliveryRepository.findAllByCustomer(customer)).thenReturn(List.of(orderDelivery));
+        when(orderDeliveryRepository.findAllByCustomer(customer)).thenReturn(List.of(orderDelivery, orderDispatched, orderEnRoute, orderConcluded, orderCanceled));
 
         List<OrderDelivery> result = sut.listAll(customer);
 
