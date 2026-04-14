adc8cb1c86b0293e3f2b36a3ee901bc8aa00a1dc
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-13 22:40:04 -0300

    feat(CreateOrderUseCase): implement validation for customer

--- src/main/java/br/ifsp/demo/application/useCases/CreateOrderUseCase.java
+++ src/main/java/br/ifsp/demo/application/useCases/CreateOrderUseCase.java
@@ -2,17 +2,29 @@ package br.ifsp.demo.application.useCases;
 
 import br.ifsp.demo.domain.aggregate.OrderDelivery;
 import br.ifsp.demo.domain.dto.CreateOrderRequest;
+import br.ifsp.demo.domain.repository.CustomerRepository;
 import br.ifsp.demo.domain.repository.OrderDeliveryRepository;
 
 
 public class CreateOrderUseCase {
     private final OrderDeliveryRepository repo;
+    private final CustomerRepository customerRepository;
 
-    public CreateOrderUseCase(OrderDeliveryRepository repo) { this.repo = repo; }
+    public CreateOrderUseCase(
+            OrderDeliveryRepository repo,
+            CustomerRepository customerRepository
+    ) {
+        this.repo = repo;
+        this.customerRepository = customerRepository;
+    }
 
     public void create(CreateOrderRequest request) {
         if (request == null) {
             throw new NullPointerException("Request cannot be null");
+        }
+
+        if (!customerRepository.exists(request.customer())) {
+            throw new IllegalArgumentException("Customer not found");
         }
 
         OrderDelivery order = new OrderDelivery(
