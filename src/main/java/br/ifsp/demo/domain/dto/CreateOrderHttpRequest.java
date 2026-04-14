286e89ae4316756662d45af2488648d2c4022a25
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-13 23:02:55 -0300

    feat(dto): create records for creation persistence

---
+++ src/main/java/br/ifsp/demo/domain/dto/CreateOrderHttpRequest.java
@@ -0,0 +1,23 @@
+package br.ifsp.demo.domain.dto;
+
+public record CreateOrderHttpRequest(
+        String customerName,
+        String customerType,
+
+        String pickupStreet,
+        String pickupNumber,
+        String pickupNeighborhood,
+        String pickupCity,
+        String pickupState,
+        String pickupCountry,
+        String pickupCep,
+
+        String deliveryStreet,
+        String deliveryNumber,
+        String deliveryNeighborhood,
+        String deliveryCity,
+        String deliveryState,
+        String deliveryCountry,
+        String deliveryCep
+) {
+}
