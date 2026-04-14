ef4e455b8cee74b29ed308bdf81af34ed49d6d0f
Filipe Braga <filipebraga0505@gmail.com>
2026-04-14 17:12:27 -0300

    feat: Implementing the method listActive Orders

--- src/main/java/br/ifsp/demo/application/useCases/ListCustomerOrdersUseCase.java
+++ src/main/java/br/ifsp/demo/application/useCases/ListCustomerOrdersUseCase.java
@@ -2,6 +2,7 @@ package br.ifsp.demo.application.useCases;
 
 import br.ifsp.demo.domain.aggregate.Customer;
 import br.ifsp.demo.domain.aggregate.OrderDelivery;
+import br.ifsp.demo.domain.aggregate.StatusOrder;
 import br.ifsp.demo.domain.repository.CustomerRepository;
 import br.ifsp.demo.domain.repository.OrderDeliveryRepository;
 
@@ -21,6 +22,20 @@ public class ListCustomerOrdersUseCase {
             throw new IllegalArgumentException("Customer not found");
         }
         return orderDeliveryRepository.findAllByCustomer(customer);
+    }
+
+    List<OrderDelivery> listActiveOrders(Customer customer){
+        if(!customerRepository.exists(customer)){
+            throw new IllegalArgumentException("Customer not found");
+        }
+        List<OrderDelivery> activeOrders = new java.util.ArrayList<>(List.of());
+        List<OrderDelivery> allOrders = orderDeliveryRepository.findAllByCustomer(customer);
+        for(int i = 0; i < allOrders.size(); i++){
+            if(allOrders.get(i).getStatus() != StatusOrder.CONCLUDED && allOrders.get(i).getStatus() != StatusOrder.CANCELED){
+                activeOrders.add(allOrders.get(i));
+            }
+        }
+        return activeOrders;
     }
 
 }
