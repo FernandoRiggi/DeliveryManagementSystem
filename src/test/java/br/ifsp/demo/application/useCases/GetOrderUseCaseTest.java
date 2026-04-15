5cf5d17b49655c0826aee3b560269f7f154b72e6
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-04-15 18:43:38 -0300

    test(GetOrderUseCaseTest): add test for retrieving current order status by ID -closes #28

---
+++ src/test/java/br/ifsp/demo/application/useCases/GetOrderUseCaseTest.java
@@ -0,0 +1,48 @@
+package br.ifsp.demo.application.useCases;
+
+import br.ifsp.demo.annotation.TDD;
+import br.ifsp.demo.domain.aggregate.OrderDelivery;
+import br.ifsp.demo.domain.aggregate.StatusOrder;
+import br.ifsp.demo.domain.repository.OrderDeliveryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class GetOrderUseCaseTest {
+
+    @Mock
+    private OrderDeliveryRepository orderDeliveryRepository;
+
+    @InjectMocks
+    private GetOrderUseCase sut;
+
+    @TDD
+    @Test
+    @DisplayName("[#28] Given customer has an order, when getById, then current status should be returned")
+    void shouldReturnCurrentStatusWhenGettingOrderById(StatusOrder currentStatus) {
+        OrderDelivery order = mock(OrderDelivery.class);
+        UUID orderId = UUID.randomUUID();
+
+        when(orderDeliveryRepository.findById(orderId)).thenReturn(Optional.of(order));
+        when(order.getStatus()).thenReturn(currentStatus);
+
+        StatusOrder response = sut.findById(orderId);
+
+        assertThat(response).isNotNull();
+        assertThat(response).isEqualTo(currentStatus);
+
+        verify(orderDeliveryRepository).findById(orderId);
+        verify(order).getStatus();
+    }
+}
