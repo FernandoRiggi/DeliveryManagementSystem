e50bf538c6a66f4309cc7fcda1385a0ba08c5013
Sabrina Andrade <eusabrinaps@icloud.com>
2026-05-07 21:12:33 -0300

    test: add mutation test to kill surviving mutants in CancelOrderUseCase

--- src/test/java/br/ifsp/demo/application/UseCases/CancelOrderUseCaseTest.java
+++ src/test/java/br/ifsp/demo/application/UseCases/CancelOrderUseCaseTest.java
@@ -1,6 +1,8 @@
 package br.ifsp.demo.application.UseCases;
 
+import br.ifsp.demo.annotation.Mutation;
 import br.ifsp.demo.annotation.TDD;
+import br.ifsp.demo.domain.aggregate.OrderDelivery;
 import br.ifsp.demo.domain.repository.OrderDeliveryRepository;
 import br.ifsp.demo.exception.OrderNotFoundException;
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +20,8 @@ import static org.mockito.Mockito.*;
 public class CancelOrderUseCaseTest {
     @Mock
     private OrderDeliveryRepository repo;
+    @Mock
+    private OrderDelivery order;
 
     @InjectMocks
     private CancelOrderUseCase sut;
@@ -32,5 +36,16 @@ public class CancelOrderUseCaseTest {
         assertThatExceptionOfType(OrderNotFoundException.class)
                 .isThrownBy(()-> sut.cancelOrderDelivery(idNotFound));
         verify(repo, never()).save(any());
+    }
+
+    @Mutation
+    @Test
+    @DisplayName("[Mutation] Should cancel order and save when order exists")
+    void ShouldCancelAndSaveOrderSuccessfully(){
+        UUID orderId = UUID.randomUUID();
+        when(repo.findById(orderId)).thenReturn(Optional.of(order));
+        sut.cancelOrderDelivery(orderId);
+        verify(order, times(1)).cancel();
+        verify(repo,times(1)).save(order);
     }
 }
