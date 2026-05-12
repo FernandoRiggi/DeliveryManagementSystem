e3e54109db6f63c9982fdf920431f84adc5a5155
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-05-11 22:45:13 -0300

    test(orderDeliveryEventTest): cover  order delivery event constructor

--- src/test/java/br/ifsp/demo/domain/event/OrderDeliveryEventTest.java
+++ src/test/java/br/ifsp/demo/domain/event/OrderDeliveryEventTest.java
@@ -18,4 +18,17 @@ class OrderDeliveryEventTest {
         assertThat(event).isNotNull();
         assertThat(event.getType()).isEqualTo(EventType.CREATED);
     }
+
+    @Mutation
+    @Test
+    @DisplayName("[Mutation] Should create event with fixed datetime")
+    void shouldCreateEventWithFixedDateTime() {
+        EventType type = EventType.CREATED;
+        LocalDateTime dateTime = LocalDateTime.of(2026, 5, 11, 10, 30);
+
+        OrderDeliveryEvent event = new OrderDeliveryEvent(type, dateTime);
+
+        assertThat(event.getType()).isEqualTo(type);
+        assertThat(event.getDateTime()).isEqualTo(dateTime);
+    }
 }
